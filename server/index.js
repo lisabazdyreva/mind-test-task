0eb0e3873ca9c7f3c479124b2eebc0e2122a0977
Lisa <lisabazdyreva@gmail.com>
2023-05-09 18:04:03 +0300

    another tr deploy app export

--- server/index.js
+++ server/index.js
@@ -14,13 +14,13 @@ const path = require("path");
 const PORT = process.env.PORT || 5000;
 
 const app = express();
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
-app.use(express.static(path.resolve(__dirname, "static")));
-
-app.use(fileUpload({}));
-
-app.use("/api", router);
+// app.use(express.static(path.resolve(__dirname, "static")));
+//
+// app.use(fileUpload({}));
+//
+// app.use("/api", router);
 
 app.get("/", (req, res) => {
   res.send("Express on Vercel");

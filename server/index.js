92d9fc1c15132f7f06641d451a25cf43353a07ad
Lisa <lisabazdyreva@gmail.com>
2023-05-09 18:24:00 +0300

    another tr deploy app export

--- server/index.js
+++ server/index.js
@@ -1,27 +1,7 @@
-// require("dotenv").config();
-
 const express = require("express");
-const sequelize = require("./db");
-const fileUpload = require("express-fileupload");
-
-const cors = require("cors");
-const bot = require("./tg-bot/bot");
-
-const router = require("./routes/index");
-const errorHandler = require("./middleware/error-handling.middleware");
-const path = require("path");
-
-const PORT = process.env.PORT || 5000;
-
 const app = express();
-// app.use(cors());
-app.use(express.json());
-// app.use(express.static(path.resolve(__dirname, "static")));
-//
-// app.use(fileUpload({}));
-//
-// app.use("/api", router);
 
+app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Express on Vercel");
 });

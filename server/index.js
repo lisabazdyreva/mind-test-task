2ba6693046af7483d949d9afc254c51d380742cc
Lisa <lisabazdyreva@gmail.com>
2023-05-09 19:15:28 +0300

    try railway to deploy

--- server/index.js
+++ server/index.js
@@ -1,36 +1,44 @@
-const express = require("express");
-const cors = require("cors");
+require("dotenv").config();
 
-const router = require("./routes/index.js");
+const express = require("express");
+const sequelize = require("./db");
+const fileUpload = require("express-fileupload");
+
+const cors = require("cors");
+const bot = require("./tg-bot/bot");
+
+const router = require("./routes/index");
+const errorHandler = require("./middleware/error-handling.middleware");
+const path = require("path");
+
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(cors());
-
 app.use(express.json());
+app.use(express.static(path.resolve(__dirname, "static")));
 
-app.use("", router);
-// app.get("/product", (req, res) => {
-//   res.json({ message: "OKKKK" });
-// });
+app.use(fileUpload({}));
 
-// app.use(errorHandler);
+app.use("/api", router);
 
-// const start = async () => {
-//   try {
-//     await sequelize.authenticate();
-//     await sequelize.sync();
-//
-//     // bot.getBot().on("message", async (msg) => {
-//     //   bot.setChatId(msg.chat.id);
-//     //
-//     //   await bot.getBot().sendMessage(bot.getChatId(), "Hello!");
-//     // });
-//
-//
-//   } catch (e) {
-//     console.log(e);
-//   }
-// };
+app.use(errorHandler);
 
-app.listen(5000, () => console.log(`Server started on port ${5000}`));
-module.exports = app;
+const start = async () => {
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
+
+    // bot.getBot().on("message", async (msg) => {
+    //   bot.setChatId(msg.chat.id);
+    //
+    //   await bot.getBot().sendMessage(bot.getChatId(), "Hello!");
+    // });
+
+    app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+start();

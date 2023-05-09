b5068e8e51dd9244d6e597560661a5ccefc3032f
Lisa <lisabazdyreva@gmail.com>
2023-05-09 19:37:20 +0300

    remove bot

--- server/index.js
+++ server/index.js
@@ -5,7 +5,6 @@ const sequelize = require("./db");
 const fileUpload = require("express-fileupload");
 
 const cors = require("cors");
-const bot = require("./tg-bot/bot");
 
 const router = require("./routes/index");
 const errorHandler = require("./middleware/error-handling.middleware");
@@ -28,12 +27,6 @@ const start = async () => {
   try {
     await sequelize.authenticate();
     await sequelize.sync();
-
-    // bot.getBot().on("message", async (msg) => {
-    //   bot.setChatId(msg.chat.id);
-    //
-    //   await bot.getBot().sendMessage(bot.getChatId(), "Hello!");
-    // });
 
     app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
   } catch (e) {
